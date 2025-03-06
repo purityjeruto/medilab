a9129dd41ca57cd0e400784133631555b1db752f
Gweshka <prtjeruto@gmail.com>
2025-03-06 10:11:46 +0300

    third commit

--- medapp/urls.py
+++ medapp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from medapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home,name='home'),
+    path('home/', views.home,name='home'),
     path('starter/', views.starter,name='starter'),
     path('about/', views.about,name='about'),
     path('services/', views.services,name='services'),
@@ -16,5 +16,7 @@ urlpatterns = [
     path('delete/<int:id>', views.delete),
 
     path('edit/<int:id>', views.edit,name='edit'),
+    path('', views.register,name='register'),
+    path('login/', views.login_view,name='login'),
 
 ]
